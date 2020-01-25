22450552bc98d038f8930cba5b282eae97578ab8
metas-mk <mark.krake@metasfresh.com>
2020-01-25 19:11:22 +0100

    Repeating example for IIFE

---
+++ nodejs/app4.js
@@ -0,0 +1,12 @@
+// IIFE example
+// 1. anonymous function
+// 2. paranthesis around it turns it into an expression
+// 3. invoking
+// = immediately invoked funtional expression
+(function() {
+  var firstname = 'John';
+  console.log(firstname);
+})();
+
+var firstname = 'Jane';
+console.log(firstname);
