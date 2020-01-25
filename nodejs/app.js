e392b3b4401d8b2220e24aaad970fbc5fc4da12c
metas-mk <mark.krake@metasfresh.com>
2020-01-25 14:59:12 +0100

    Adding some first steps with nodejs.

---
+++ nodejs/app.js
@@ -0,0 +1,24 @@
+// function statement
+function greet() {
+  console.log('Hi');
+}
+greet();
+// functions are first-class
+function logGreeting(fn) {
+  fn();
+}
+logGreeting(greet);
+
+// function expression
+var greetMe = function() {
+  console.log('Hi Mark');
+};
+greetMe();
+
+// it's first class
+logGreeting(greetMe);
+
+// use a function expression on the fly
+logGreeting(function() {
+  console.log('Hello Mark');
+});
